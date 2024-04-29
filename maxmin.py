f001ac32a8c7a5c0bae71c3dbac6c4d850839be9
Jeremiah Barte <jeremiahbarte@gmail.com>
2024-04-29 10:40:15 +0100

    fixed the name of variables and added some code comments for clarity

--- maxmin.py
+++ maxmin.py
@@ -1,6 +1,20 @@
 user_input = input("please enter a list of numbers separated by ','\n")
 user_list = user_input.split(",")
 
+"""
+under the assumption of a non empty list and a limited range of list values
+we will find the largest and the smallest items of the list
+
+method:
+if bigggest - number is less than zero, number must be bigger than our current biggest
+is smaller than the current number we are looking at
+
+conversely, if smallest - number is more than zero, our current smallest number is 
+larger than this number we are currently on
+
+we end the algorithm once we have looked at every item 
+"""
+
 biggest = -99999
 smallest = 99999
 
@@ -10,4 +24,4 @@ for item in user_list:
         biggest = number
     if smallest - number >= 0:
         smallest = number
-print(f"the smallest number is {smallest} and the largest is {largest}\n")
+print(f"the smallest number is {smallest} and the largest is {biggest}\n")
